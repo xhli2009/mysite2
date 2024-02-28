93757c97368f6b6a10952ce9402b3d853128609b
xhli2009 <xhli2009@163.com>
2024-02-28 11:15:12 +0800

    init repo mysite
    init repo

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import datetime
 
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Question
 # Create your tests here.
+
+def create_question(question_text,days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now()+datetime.timedelta(days=days)
+    return Question.objects.create(Question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_quesions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"No polls are available.")
+        #self.assertQuerysetEqual(response.context["lastest_question_list"],[])
+        
+    def test_future_question(self):
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        #self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_pulished_recently(), False)
+        
+        
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_pulished_recently(), False)
+        
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_pulished_recently(), True)
+        
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 #from django.template import loader
 #from django.shortcuts import render
 
@@ -13,6 +16,23 @@ def index(request):
 #    template = loader.get_template("polls/index.html")
     context = {"latest_question_list":latest_questin_list,}
     return render(request,"polls/index.html",context)
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name ="latest_question_list"
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        #return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def detail(request,question_id):
 #    try:
